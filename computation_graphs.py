0a8aee253d3bad5e52d7ec88ff592c737ea875bf
xmq <xmqsysd@outlook.com>
2022-06-01 21:28:46 +0800

    Add backward

--- computation_graphs.py
+++ computation_graphs.py
@@ -1,22 +1,23 @@
+import numpy as np
 
 
-class Add:
+class AddLayer:
     def __init__(self):
-        self.x = None
-        self.y = None
+        pass
 
-    def forward(self, in_x, in_y):
-        self.x = in_x
-        self.y = in_y
-        return self.x + self.y
+    @staticmethod
+    def forward(in_x, in_y):
+        out = in_x + in_y
+        return out
 
-    def backward(self, diff_out):
+    @staticmethod
+    def backward(diff_out):
         dx = diff_out * 1
         dy = diff_out * 1
         return dx, dy
 
 
-class Multiple:
+class MultiLayer:
     def __init__(self):
         self.x = None
         self.y = None
@@ -24,9 +25,25 @@ class Multiple:
     def forward(self, in_x, in_y):
         self.x = in_x
         self.y = in_y
-        return self.x * self.y
+        return np.dot(self.x, self.y)
 
     def backward(self, diff_out):
-        dx = diff_out * self.y
-        dy = diff_out * self.x
+        dx = np.dot(diff_out, np.transpose(self.y))
+        dy = np.dot(np.transpose(self.x), diff_out)
         return dx, dy
+
+
+class ReLULayer:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, in_x):
+        self.mask = (in_x <= 0)
+        out = in_x.copy()
+        out[self.mask] = 0
+        return out
+
+    def backward(self, diff_out):
+        diff_out[self.mask] = 0
+        dx = diff_out
+        return dx
--- deep-learning.py
+++ deep-learning.py
@@ -9,17 +9,28 @@ from computation_graphs import *
 
 
 class Network:
-    def __init__(self, input_shape=2, output_shape=3, activity_func=Functions.softmax, identity_func=Functions.softmax):
+    def __init__(self, input_shape=2, output_shape=3, learning_rate=0.1, activity_func=Functions.softmax, identity_func=Functions.softmax):
         self.input_shape = input_shape
         self.output_shape = output_shape
+        self.learning_rate = learning_rate
 
-        self.network = {'W1': np.random.rand(input_shape, 3)-0.5,
-                        'b1': np.random.rand(3)-0.5,
-                        'W2': np.random.rand(3, 4)-0.5,
-                        'b2': np.random.rand(4)-0.5,
-                        'W3': np.random.rand(4, output_shape)-0.5,
-                        'b3': np.random.rand(output_shape)-0.5
+        self.network = {'W1': np.random.rand(input_shape, 3) - 0.5,
+                        'b1': np.random.rand(3) - 0.5,
+                        'W2': np.random.rand(3, 4) - 0.5,
+                        'b2': np.random.rand(4) - 0.5,
+                        'W3': np.random.rand(4, output_shape) - 0.5,
+                        'b3': np.random.rand(output_shape) - 0.5
                         }
+
+        self.graphs = {'M1': MultiLayer(),
+                       'A1': AddLayer(),
+                       'R1': ReLULayer(),
+                       'M2': MultiLayer(),
+                       'A2': AddLayer(),
+                       'R2': ReLULayer(),
+                       'M3': MultiLayer(),
+                       'A3': AddLayer(),
+                       'R3': ReLULayer()}
 
         self.activity_func = activity_func
         self.identity_func = identity_func
@@ -30,6 +41,18 @@ class Network:
     def identity_function(self, inputs):
         return self.identity_func(inputs)
 
+    def graph_forward(self, inputs):
+        m1 = self.graphs['M1'].forward(inputs, self.network['W1'])
+        a1 = self.graphs['A1'].forward(m1, self.network['b1'])
+        r1 = self.graphs['R1'].forward(a1)
+        m2 = self.graphs['M2'].forward(r1, self.network['W2'])
+        a2 = self.graphs['A2'].forward(m2, self.network['b2'])
+        r2 = self.graphs['R2'].forward(a2)
+        m3 = self.graphs['M3'].forward(r2, self.network['W3'])
+        a3 = self.graphs['A3'].forward(m3, self.network['b3'])
+        r3 = self.graphs['R3'].forward(a3)
+        return r3
+
     def forward(self, inputs):
         a1 = np.dot(inputs, self.network['W1']) + self.network['b1']
         z1 = self.activity_function(a1)
@@ -39,13 +62,30 @@ class Network:
         outputs = self.identity_function(a3)
         return outputs
 
-    def backward(self):
-        pass
+    def backward(self, predicts, labels):
+        diff = predicts - labels
+        diff_out = diff * diff
+        diff_r3 = self.graphs['R3'].backward(diff_out)
+        diff_a3, diff_b3 = self.graphs['A3'].backward(diff_r3)
+        diff_m3, diff_w3 = self.graphs['M3'].backward(diff_a3)
+        diff_r2 = self.graphs['R2'].backward(diff_m3)
+        diff_a2, diff_b2 = self.graphs['A2'].backward(diff_r2)
+        diff_m2, diff_w2 = self.graphs['M2'].backward(diff_a2)
+        diff_r1 = self.graphs['R1'].backward(diff_m2)
+        diff_a1, diff_b1 = self.graphs['A1'].backward(diff_r1)
+        diff_m1, diff_w1 = self.graphs['M1'].backward(diff_a1)
+
+        self.network['W3'] -= diff_w3 * self.learning_rate
+        self.network['b3'] -= np.sum(diff_b3, axis=0) * self.learning_rate
+        self.network['W2'] -= diff_w2 * self.learning_rate
+        self.network['b2'] -= np.sum(diff_b2, axis=0) * self.learning_rate
+        self.network['W1'] -= diff_w1 * self.learning_rate
+        self.network['b1'] -= np.sum(diff_b1, axis=0) * self.learning_rate
 
     @staticmethod
     def print_correct_rate(predicts, labels):
         corrects = np.sum(predicts == labels)
-        correct_rate = corrects/labels.shape[0]
+        correct_rate = corrects / labels.shape[0]
         print("correct rate: {rate:.2%}".format(rate=correct_rate))
         return correct_rate
 
@@ -89,9 +129,18 @@ def main():
     image_batch = np.reshape(train_images[0:batch_size], [batch_size, image_shape])
     label_batch = np.array(train_labels[0:batch_size])
     net = Network(input_shape=image_shape, output_shape=10)
-    y = net.forward(image_batch)
+    y = net.graph_forward(image_batch)
     predict = np.argmax(y, axis=1)
     Network.print_correct_rate(predicts=predict, labels=label_batch)
+
+    labels_one_hot = np.zeros(shape=(batch_size, 10))
+    for i in range(0, batch_size):
+        labels_one_hot[i][label_batch[i]] = 1
+
+    net.backward(y, labels_one_hot)
+    z = net.graph_forward(image_batch)
+    predict_2 = np.argmax(z, axis=1)
+    Network.print_correct_rate(predicts=predict_2, labels=label_batch)
 
 
 if __name__ == '__main__':
@@ -100,4 +149,4 @@ if __name__ == '__main__':
     main()
 
     end = time.time()
-    print("total run time: {time:.4f}".format(time=end-start))
+    print("total run time: {time:.4f}".format(time=end - start))
